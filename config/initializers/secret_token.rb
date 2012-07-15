eba9c81b4b8ba3c502358eb2308788e10365bd9b
David M <dg.mendez67@uniandes.edu.co>
2012-07-15 11:30:45 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProtoEClouds::Application.config.secret_token = 'cff12d5b5074e1752ca38d5a6e0d4ef5c69df68796f2fd2826009c2c09d948d80afabf682dd7cb199f9963727cfb297677e080c416f3987f5ab7448f5ae18475'
